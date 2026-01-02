13c67f0ef4434b7afd76eed819480f9182e569fa
ArnavMahajan01 <arnav.mahajan.2001@gmail.com>
2026-01-02 17:49:03 +1100

    Implemented Homepage with CHarts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
+import Header from "@/components/Header";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="min-h-screen text-grey-400">
       {/* ADDING HEADER HERE BECAUSE WE WANT EVERY SINGLE PAGE INSIDE ROOT TO HAVE A HEADER */}
-      {/* Header */}
-      <div className="">{children}</div>
-      {/* <div className="container py-10">{children}</div> */}
+      <Header />
+      {/* <div className="">{children}</div> */}
+      <div className="container py-10">{children}</div>
     </main>
   );
 };
